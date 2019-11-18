2ac7675cb22db9ee6c642aab42f1f3131912f987
Anton gusev <agus179e@gmail.com>
2019-11-18 09:55:00 +0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, url_for, request
+from flask import render_template
+import sqlite3
+
+DATABASE = "example.db"
+TYPES = ["Simple test"]
+
+app = Flask(__name__,
+            static_folder="static")
+
+
+@app.route('/')
+def home_page():
+    return render_template('homepage.html', name="Anton")
+
+
+@app.route('/admin', methods=["GET", 'POST'])
+def admin_panel():
+    a = sqlite3.connect("example.db")
+    c = a.cursor()
+    c.execute("select * from tests")
+    results = c.fetchall()
+    if request.method == 'POST':
+        print('sender')
+        print('{} {}'.format(request.form['test_name'], request.form['test_type']))
+        c.execute('''insert into tests 
+        (name, type) values
+        ("{}",{})'''.format(request.form['test_name'], request.form['test_type']))
+    a.commit()
+    a.close()
+    return render_template('adminpanel.html', results=results, types=TYPES)
+
+
+@app.route('/play')
+def play_test():
+    return render_template('test.html')
+
+
+if __name__ == '__main__':
+    app.run()
+    a.close()
