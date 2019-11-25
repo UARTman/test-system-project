57486fa4aec16b661285832002803c8d4f73acec
Anton gusev <agus179e@gmail.com>
2019-11-25 07:32:34 +0300

    Fully applied ORM

--- app.py
+++ app.py
@@ -5,13 +5,7 @@ from orm import *
 
 import sqlite3
 
-DATABASE = "example.db"
 TYPES = ["Simple test"]
-TEST_MODEL_SQL = '''select text_test_questions.question_number, text_test_answers.answer_number, correct_answer, question, answer
-from tests, text_test_answers, text_test_questions
-where text_test_questions.test_id = id and text_test_answers.test_id = id and id = {}
-and text_test_answers.question_number = text_test_questions.question_number
-order by text_test_questions.question_number'''
 
 app = Flask(__name__,
             static_folder="static")
@@ -44,20 +38,6 @@ def restricted(func, users=None):
     return wrapper
 
 
-def with_sql(func, db="example.db"):
-    def wrapper(*args, **kwargs):
-        a = sqlite3.connect(db)
-        c = a.cursor()
-        kwargs['cursor'] = c
-        ret = func(*args, **kwargs)
-        a.commit()
-        a.close()
-        return ret
-
-    wrapper.__name__ = func.__name__
-    return wrapper
-
-
 @app.route('/access_denied')
 def access_denied():
     return "<h1>Access Denied!</h1> <a href='{0}'> Home </a> ".format(url_for("home_page"))
@@ -80,7 +60,7 @@ def home_page():
     return render_template('homepage.html', name="Anton")
 
 
-@app.route('/add_test', methods=["GET", "POST"])
+@app.route('/add_test', methods=["POST"])
 @restricted
 def add_test():
     if request.method == 'POST':
@@ -89,18 +69,11 @@ def add_test():
     return redirect("/admin")
 
 
-@app.route('/remove_test', methods=["GET", "POST"])  # TODO: orm-ify remove_test
+@app.route('/remove_test', methods=["POST"])
 @restricted
-@with_sql
-def remove_test(cursor=None):
-    if request.method == 'POST':
-        cursor.execute("delete from tests where id={}"
-                       .format(request.form['test_id']))
-        cursor.execute("delete from text_test_questions where test_id={}"
-                       .format(request.form['test_id']))
-        cursor.execute("delete from text_test_answers where test_id={}"
-                       .format(request.form['test_id']))
-        print('confirm')
+def remove_test():
+    with db.atomic():
+        Test.get_by_id(int(request.form['test_id'])).delete_instance(recursive=True)
     return redirect("/admin")
 
 
@@ -120,58 +93,35 @@ def admin_test(ident):
     return render_template("admin_text_test.html", model=questions)
 
 
-@app.route('/play')  # TODO: orm-ify play_test
-@with_sql
-def play_test(cursor=None):
-    cursor.execute("select * from tests")
-    results = cursor.fetchall()
+@app.route('/play')
+def play_test():
+    with db.atomic():
+        results = Test.select()
     return render_template('tests_page.html', results=results, types=TYPES)
 
 
-@app.route('/play/<ident>')  # TODO: orm-ify take_test
-@with_sql
-def take_test(ident, cursor=None):
-    cursor.execute(TEST_MODEL_SQL.format(ident))
-    results = cursor.fetchall()
-    model = []
-    last = None
-    for i in results:
-        if last != i[0]:
-            last = i[0]
-            model.append([i[0], i[3], [], i[2]])
-        model[-1][2].append([i[1], i[4]])
-    return render_template("test_participate_text.html", model=model, id=ident)
+@app.route('/play/<int:ident>')
+def take_test(ident):
+    with db.atomic():
+        questions = Test.get_by_id(ident).questions
+    return render_template("test_participate_text.html", model=questions, id=ident)
 
 
-@app.route('/play/<ident>/evaluate', methods=["GET", "POST"])  # TODO: orm-ify eval_test
-@with_sql
-def eval_test(ident, cursor=None):
-    cursor.execute(TEST_MODEL_SQL.format(ident))
-    results = cursor.fetchall()
-    model = []
-    last = None
-    for i in results:
-        if last != i[0]:
-            last = i[0]
-            model.append([i[0], i[3], [], i[2]])
-        model[-1][2].append([i[1], i[4]])
-    l = len(request.form) - 1
-    print(request.form)
-    print(model)
-    print(l)
-    model1 = []
+@app.route('/play/<int:ident>/evaluate', methods=["POST"])
+def eval_test(ident):
+    answers = {}
+    for i in request.form:
+        if i[0] == 'q':
+            answers[int(i[1:])] = int(request.form[i])
     c = 0
-    for i in model:
-        for j in i[2]:
-            if j[0] == i[-1]:
-                k = j[1]
-            if j[0] == int(request.form["q{}".format(i[0])]):
-                ll = j[1]
-        if k == ll:
+
+    with db.atomic():
+        questions = Test.get_by_id(ident).questions
+    for i in questions:
+        if i.correct_answer == answers[i.number]:
             c += 1
-        model1.append([i[0], i[1], ll, k])
-    print(model1, c, l)
-    return render_template("test_evaluate_text.html", model=model1, correct=c, length=l)
+    print(answers, c)
+    return render_template("test_evaluate_text.html", model=questions, answers=answers, correct=c, length=len(questions))
 
 
 if __name__ == '__main__':
